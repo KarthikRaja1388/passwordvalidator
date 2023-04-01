e44d6c51e43b4dcbd643f64929c7bd8b52065af0
karthik <karthik.vbdon88@gmail.com>
2023-04-01 14:38:21 +1300

    initial commit

--- main.js
+++ main.js
@@ -4,11 +4,32 @@ let cpwd = document.querySelector(".cpwd");
 let btn = document.querySelector(".btn");
 
 btn.addEventListener("click", () => {
-  if (pwd.value.isEmpty() || cpwd.value.isEmpty()) {
-    display.innerHTML = "Password is empty";
-  } else if (pwd.value === cpwd.value) {
-    display.innerHTML = "Password Matched";
+  let pwdValue = pwd.value;
+  let cpwdValue = cpwd.value;
+
+  if (pwdValue !== "" || cpwdValue !== "") {
+    if (!pwdValue.length > 8 || !cpwdValue.length > 8) {
+      if (pwdValue === cpwdValue) {
+        display.innerHTML = "Password matched!";
+        pwd.classList.add("match");
+        cpwd.classList.add("match");
+        display.style.color = "green";
+      } else {
+        display.innerHTML = "Password not matching";
+        applyErrorStyle();
+      }
+    } else {
+      display.innerHTML = "Password must have minimum of 8 characters";
+      applyErrorStyle();
+    }
   } else {
-    display.innerHTML = "Password is not matching";
+    display.innerHTML = "Password can't be empty";
+    applyErrorStyle();
   }
 });
+
+function applyErrorStyle() {
+  pwd.classList.add("mismatch");
+  cpwd.classList.add("mismatch");
+  display.style.color = "red";
+}
